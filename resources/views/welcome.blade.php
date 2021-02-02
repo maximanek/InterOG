43cafa9c766a15893c1b16eca3fb6118a071ef09
maximanek <maxzieba@student.agh.edu.pl>
2021-02-02 09:49:00 +0100

    Git innit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Wybierz funkcjonalność</div>
+                        <div>
+                            <a href="/old/allegro">Allegro</a> <br>
+                            <a href="/old/sklep">Sklep</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
